a40977fcbb72225bcb1fa88c0f1cf70e058a0fef
ke.yi <ke.yi@sophgo.com>
2025-02-18 15:35:39 +0800

    89d39089b6d,fix ut bug.

--- v2/self_test/audio/audio_pcm/cvi_sample_audio.c
+++ v2/self_test/audio/audio_pcm/cvi_sample_audio.c
@@ -3713,7 +3713,7 @@ CVI_S32 main(int argc, char *argv[])
 	}
 	case 16: {
 		printf("[cviaudio] IOCTL Test!\n");
-		SAMPLE_AUDIO_DEBUG_IOCTL_TEST();
+		printf("IOCTL Test is no longer supported\n");
 		printf("[cviaudio]IOCTL Test!...end\n");
 		break;
 	}
--- v2/self_test/hdmi_ut/hdmi_ut_main.c
+++ v2/self_test/hdmi_ut/hdmi_ut_main.c
@@ -803,6 +803,9 @@ static CVI_S32 _hdmi_ut_handle_op(CVI_S32 op)
 	}
 
 	case 110: {
+		char private_data[30] = "Test Private_Data";
+		callback_func.hdmi_event_callback = Hdmi_EventProc;
+		callback_func.private_data = private_data;
 		s32Ret = CVI_HDMI_UnRegisterCallback(&callback_func);
 		if(s32Ret){
 			printf("HDMI UnRegisterCallback error\n");
